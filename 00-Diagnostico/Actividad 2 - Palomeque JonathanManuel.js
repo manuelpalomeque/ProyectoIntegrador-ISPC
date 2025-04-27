35204faeb973d34eb0dfee4b9a262adeeee2d1d6
Jonathan Manuel Palomeque <87273052+manuelpalomeque@users.noreply.github.com>
2025-04-27 13:51:54 -0300

    Add files via upload

---
+++ 00-Diagnostico/Actividad 2 - Palomeque JonathanManuel.js
@@ -0,0 +1,6 @@
+// 1. Desestructuración 
+const { nombre, edad, profesion } = persona;
+console.log(`${nombre} tiene ${edad} años y trabaja como ${profesion}.`);
+
+// 2. Agregar nueva propiedad "ciudad"
+persona.ciudad = "Rosario";
